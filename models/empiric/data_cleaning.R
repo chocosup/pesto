26c89380520db8cbb197121ad26519ce825e4f9b
Ambroise <ambroise.marigot@mines.org>
2015-10-30 15:59:08 +0100

    consecutive function

--- models/empiric/data_cleaning.R
+++ models/empiric/data_cleaning.R
@@ -13,3 +13,18 @@ for(HTA_name in HTA_names)
 HTA_ok = as.character(colnames(pourcentages_zero[1, pourcentages_zero < 0.05]))
 NewConsoMeas = ConsoMeasParDepart[,HTA_ok]
 
+consecutive <- function(x)
+{
+  a = 0
+  b = 0
+  for(i in 1:length(x))
+  {
+    if(x[i])
+    {
+      a = a+1
+      if(a > b){b = a}
+    }
+    else{a = 0}
+  }
+  return(b)
+}
